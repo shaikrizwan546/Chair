887799fb4ef8943c98c2ab11abe29c3632e4467c
rizwa <rizwa@RIZWAN>
2023-02-23 19:53:11 +0530

    Add project files.

---
+++ Chair/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace Chair
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.TopDiaTextBox1 = new System.Windows.Forms.TextBox();
+            this.TopDiameter = new System.Windows.Forms.Label();
+            this.BottomDiaTextBox2 = new System.Windows.Forms.TextBox();
+            this.BottomDiameter = new System.Windows.Forms.Label();
+            this.StiffCountTextBox1 = new System.Windows.Forms.TextBox();
+            this.StiffCount = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(102, 197);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 58);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Create Chair";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // TopDiaTextBox1
+            // 
+            this.TopDiaTextBox1.Location = new System.Drawing.Point(129, 35);
+            this.TopDiaTextBox1.Name = "TopDiaTextBox1";
+            this.TopDiaTextBox1.Size = new System.Drawing.Size(100, 22);
+            this.TopDiaTextBox1.TabIndex = 1;
+            this.TopDiaTextBox1.Text = "3520";
+            // 
+            // TopDia
+            // 
+            this.TopDiameter.AutoSize = true;
+            this.TopDiameter.Location = new System.Drawing.Point(35, 41);
+            this.TopDiameter.Name = "TopDia";
+            this.TopDiameter.Size = new System.Drawing.Size(56, 16);
+            this.TopDiameter.TabIndex = 2;
+            this.TopDiameter.Text = "Top Dia";
+            // 
+            // BottomDiaTextBox2
+            // 
+            this.BottomDiaTextBox2.Location = new System.Drawing.Point(129, 87);
+            this.BottomDiaTextBox2.Name = "BottomDiaTextBox2";
+            this.BottomDiaTextBox2.Size = new System.Drawing.Size(100, 22);
+            this.BottomDiaTextBox2.TabIndex = 3;
+            this.BottomDiaTextBox2.Text = "3520";
+            // 
+            // BottomDia
+            // 
+            this.BottomDiameter.AutoSize = true;
+            this.BottomDiameter.Location = new System.Drawing.Point(35, 93);
+            this.BottomDiameter.Name = "BottomDia";
+            this.BottomDiameter.Size = new System.Drawing.Size(73, 16);
+            this.BottomDiameter.TabIndex = 4;
+            this.BottomDiameter.Text = "Bottom Dia";
+            // 
+            // StiffCountTextBox1
+            // 
+            this.StiffCountTextBox1.Location = new System.Drawing.Point(129, 146);
+            this.StiffCountTextBox1.Name = "StiffCountTextBox1";
+            this.StiffCountTextBox1.Size = new System.Drawing.Size(100, 22);
+            this.StiffCountTextBox1.TabIndex = 5;
+            this.StiffCountTextBox1.Text = "2";
+            // 
+            // StiffCount
+            // 
+            this.StiffCount.AutoSize = true;
+            this.StiffCount.Location = new System.Drawing.Point(35, 149);
+            this.StiffCount.Name = "StiffCount";
+            this.StiffCount.Size = new System.Drawing.Size(90, 16);
+            this.StiffCount.TabIndex = 6;
+            this.StiffCount.Text = "Stiffener count";
+            this.StiffCount.Click += new System.EventHandler(this.StiffCount_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(290, 283);
+            this.Controls.Add(this.StiffCount);
+            this.Controls.Add(this.StiffCountTextBox1);
+            this.Controls.Add(this.BottomDiameter);
+            this.Controls.Add(this.BottomDiaTextBox2);
+            this.Controls.Add(this.TopDiameter);
+            this.Controls.Add(this.TopDiaTextBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox TopDiaTextBox1;
+        private System.Windows.Forms.Label TopDiameter;
+        private System.Windows.Forms.TextBox BottomDiaTextBox2;
+        private System.Windows.Forms.Label BottomDiameter;
+        private System.Windows.Forms.TextBox StiffCountTextBox1;
+        private System.Windows.Forms.Label StiffCount;
+    }
+}
+
---
+++ Chair/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tekla.Structures.Model;
+using Tekla.Structures.Geometry3d;
+using Point = Tekla.Structures.Geometry3d.Point;
+using System.Collections;
+
+namespace Chair
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        double TopDia;
+        double BottomDia;
+        public void button1_Click(object sender, EventArgs e)
+        {
+           TopDia = double.Parse(TopDiaTextBox1.Text);
+           BottomDia = double.Parse(BottomDiaTextBox2.Text);
+            SetStackData();
+            Stack();
+
+            
+
+        }
+
+        public void SetStackData()
+        {
+
+
+            CreateChair();
+        }
+
+        private void CreateChair()
+        {
+            Tekla.Structures.Model.Model model = new Model();
+            double TopRad = Convert.ToDouble(TopDiaTextBox1.Text) /2;
+            double BotRad = Convert.ToDouble(BottomDiaTextBox2.Text)/ 2;
+            int stiffenerCount = Convert.ToInt32(StiffCountTextBox1.Text);
+
+            for (int i = 0; i < 4; i++)
+            {
+                ContourPoint origin = new ContourPoint(new Point(0,0,0), null);
+                ContourPoint ePoint = new ContourPoint(ShiftHorizontallyRad(origin, 100, i + 1,0), null);
+
+                CustomPart CPart = new CustomPart();
+                CPart.Name = "Cha";
+                CPart.Number = BaseComponent.CUSTOM_OBJECT_NUMBER;
+                CPart.Position.Plane = Position.PlaneEnum.LEFT;
+                CPart.Position.PlaneOffset = -10;
+                CPart.Position.Depth = Position.DepthEnum.MIDDLE;
+                CPart.Position.DepthOffset = 550;
+                CPart.Position.RotationOffset = 0;
+                CPart.Position.Rotation = Position.RotationEnum.TOP;
+                CPart.SetInputPositions(origin, ePoint);
+
+
+                CPart.SetAttribute("P1", TopRad);
+                CPart.SetAttribute("P2", BotRad);
+                CPart.SetAttribute("P3", 200);
+                CPart.SetAttribute("Ring Width", 1000);
+                CPart.SetAttribute("Stiffener Length", 1000);
+                CPart.SetAttribute("Top Ring Thickness", 50);
+                CPart.SetAttribute("Bottom Ring Thickness", 50);
+                CPart.SetAttribute("Stiffener Plate Thickness", 20);
+                CPart.SetAttribute("P9", stiffenerCount);
+                CPart.SetAttribute("Plate Washer Thickness", 16);
+                CPart.SetAttribute("Anchor Bolt Circle Distance", 2260);
+                CPart.SetAttribute("Projection", 1410);
+                CPart.SetAttribute("Embedment", 210);
+                CPart.SetAttribute("Plate washer Length", 80);
+                CPart.Insert();
+                
+                model.CommitChanges();
+
+
+            }
+
+
+        }
+        public ContourPoint ShiftHorizontallyRad
+           (ContourPoint point, double dist, int side, double angle = double.NaN)
+        {
+            ContourPoint shiftedPt;
+            if (double.IsNaN(angle))
+            {
+              
+            }
+
+            switch (side)
+            {
+                case 1:
+                    shiftedPt = new ContourPoint(new Point(
+                    point.X + (dist * Math.Cos(angle)),
+                    point.Y + (dist * Math.Sin(angle)),
+                    point.Z), null);
+                    break;
+                case 2:
+                    shiftedPt = new ContourPoint(new Point(
+                    point.X - (dist * Math.Sin(angle)),
+                    point.Y + (dist * Math.Cos(angle)),
+                    point.Z), null);
+                    break;
+                case 3:
+                    shiftedPt = new ContourPoint(new Point(
+                    point.X - (dist * Math.Cos(angle)),
+                    point.Y - (dist * Math.Sin(angle)),
+                    point.Z), null);
+                    break;
+                case 4:
+                    shiftedPt = new ContourPoint(new Point(
+                    point.X + (dist * Math.Sin(angle)),
+                    point.Y - (dist * Math.Cos(angle)),
+                    point.Z), null);
+                    break;
+                default:
+                    shiftedPt = point;
+                    break;
+            }
+
+
+            return shiftedPt;
+        }
+
+        public void Stack()
+        {
+            string pro = "CHS" + BottomDia + "*" + TopDia + "*10";
+            Tekla.Structures.Model.Model model = new Model();
+            Point point = new Point(0, 0, 0);
+            Point point2 = new Point(0, 0, 1000);
+            Beam beam = new Beam();
+            beam.StartPoint = point;
+            beam.EndPoint = point2;
+            beam.Profile.ProfileString = pro;
+            beam.Finish = "PAINT";
+            beam.Position.Plane = Position.PlaneEnum.MIDDLE;
+            beam.Position.Rotation = Position.RotationEnum.TOP;
+            beam.Position.Depth = Position.DepthEnum.MIDDLE;
+            beam.StartPointOffset = new Offset();
+            beam.EndPointOffset = new Offset();
+            bool result = false;
+            result = beam.Insert();
+            model.CommitChanges();  
+        }
+
+        private void StiffCount_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
